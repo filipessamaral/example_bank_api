747178ad0dccac2ed70ef38fe99a2eb1d371d11c
Filipe <filipe@payflow.es>
2023-11-05 21:44:50 +0100

    change workflows

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export const preset = 'ts-jest';
 export const testEnvironment = 'node';
 export const transform = {
-    '^.+\\.ts?$': 'ts-jest',
+  '^.+\\.ts?$': 'ts-jest',
 };
 export const transformIgnorePatterns = ['<rootDir>/node_modules/'];
